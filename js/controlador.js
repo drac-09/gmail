a78ced35714605ee693717119493c84ca2463fa4
DRAC-09 <allan.estrada9@gmail.com>
2022-07-10 21:39:17 -0600

    First Commit

---
+++ js/controlador.js
@@ -0,0 +1,565 @@
+var recibidos = [
+     {
+          emisor: 'Roy Mustang',
+          correoEmisor: 'roy@gmail.com',
+          asunto: 'Full Metal Alchemist',
+          mensaje: 'El mundo no es perfecto. Pero sigue presente para nosotros, y está haciendo su mejor esfuerzo. Eso es lo que lo hace tan hermoso',
+          hora: '10:00am',
+          leido: true,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Armin Arlert',
+          correoEmisor: 'armin@gmail.com',
+          asunto: 'Shingeki no Kyojin',
+          mensaje: 'Una persona que no puede sacrificar nada, no puede cambiar nada',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Dr. Hiruluk',
+          correoEmisor: 'dr.Hiruluk@gmail.com',
+          asunto: 'One Piece',
+          mensaje: 'Dime, ¿qué crees tú que es la muerte? ¿Un balazo en mitad del corazón? No. ¿Una enfermedad que consuma el cuerpo? Tampoco. ¿Un veneno que corrompa la sangre? No, señor. La muerte es cuando el mundo te olvida.',
+          hora: '10:00am',
+          leido: true,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Gildarts Clive',
+          correoEmisor: 'gildarts@gmail.com',
+          asunto: 'Fairy Tail',
+          mensaje: 'El miedo no es malo. Te dice cuál es tu debilidad. Y una vez que conozcas tu debilidad, podrás volverte más fuerte, y más amable',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Saitama',
+          correoEmisor: 'saitama@gmail.com',
+          asunto: 'One-Punch Man',
+          mensaje: 'Dejaré los problemas de mañana al yo del mañana',
+          hora: '10:00am',
+          leido: false,
+          destacado: true,
+          spam: false
+     },
+     {
+          emisor: 'Natsu Dragneel',
+          correoEmisor: 'natsu@gmail.com',
+          asunto: 'Fairy Tail',
+          mensaje: '¡No tenemos que saber lo que nos depara el mañana! ¡Es por eso que podemos vivir por todo lo que valemos hoy!',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Killua',
+          correoEmisor: 'kilua@gmail.com',
+          asunto: 'Hunter x Hunter',
+          mensaje: 'La estupidez es mucho más fascinante que la inteligencia. La inteligencia tiene límites, la estupidez no.',
+          hora: '10:00am',
+          leido: false,
+          destacado: true,
+          spam: false
+     },
+     {
+          emisor: 'Gintoki',
+          correoEmisor: 'gintoki@gmail.com',
+          asunto: 'Gintama',
+          mensaje: 'Recuerda, en nuestra vida nosotros no somos lectores, sino escritores. ',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Kaiki Deishuu',
+          correoEmisor: 'kaiki@gmail.com',
+          asunto: 'Monogatari Series',
+          mensaje: 'Si realmente deseas algo, si realmente quieres que se realice, nunca debes poner ese deseo en palabras.',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Vash Estampida',
+          correoEmisor: 'vash@gmail.com',
+          asunto: 'Trigun',
+          mensaje: 'La gente tiene diferentes formas de pensar, incluso cuando se comete un error... Si la persona se da cuenta de su error es posible que lo enmiende, si mantienes tu visión clara verás el futuro, es de lo que se trata esta vida…',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Erwin Smith',
+          correoEmisor: 'erwin@gmail.com',
+          asunto: 'Shingeki no Kyojin',
+          mensaje: 'Si empiezas a arrepentirte, entorpecerás tus decisiones futuras y dejarás que otros tomen tus decisiones por ti. Lo único que te queda entonces es morir. Nadie puede predecir el resultado. Cada decisión que tomas sólo tiene sentido al afectar a tu siguiente decisión. ',
+          hora: '10:00am',
+          leido: false,
+          destacado: true,
+          spam: false
+     },
+     {
+          emisor: 'Vegetta',
+          correoEmisor: 'vegeta@gmail.com',
+          asunto: 'Dragon Ball',
+          mensaje: 'No me vencerás porque soy el príncipe de los Saiyajins.',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Gintoki',
+          correoEmisor: 'gintoki@gmail.com',
+          asunto: 'Gintama',
+          mensaje: 'No peleo para tener una ofrenda en mi tumba, sino por el desayuno de mañana. ',
+          hora: '10:00am',
+          leido: false,
+          destacado: true,
+          spam: false
+     },
+     {
+          emisor: 'Ichigo Kurosaki',
+          correoEmisor: 'ichigok@gmail.com',
+          asunto: 'Bleach',
+          mensaje: 'Si los milagros solo ocurren una vez, ¿cómo se llama a la segunda?',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Vegetta',
+          correoEmisor: 'vegeta@gmail.com',
+          asunto: 'Dragon Ball',
+          mensaje: 'Insecto',
+          hora: '10:02am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Monkey D. Luffy',
+          correoEmisor: 'luffy@gmail.com',
+          asunto: 'One Piece',
+          mensaje: 'Me llamo Monkey D. Luffy, ¡¡y voy a ser el próximo rey de los piratas!!',
+          hora: '5:00pm',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Katsura Kotaro',
+          correoEmisor: 'tsura@gmail.com',
+          asunto: 'Gintama',
+          mensaje: 'No es Tsura, es Katsura',
+          hora: '00:01am',
+          leido: false,
+          destacado: true,
+          spam: false
+     },
+     {
+          emisor: 'Anonimo',
+          correoEmisor: 'suport@gmail.com',
+          asunto: 'Sin asunto',
+          mensaje: 'Hay 3 clases de personas, las que pueden contar y las que no',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Anuncio',
+          correoEmisor: 'anuncio@gmail.com',
+          asunto: 'Promociones',
+          mensaje: 'Meses sin intereses',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: true
+     },
+     {
+          emisor: 'Anuncio',
+          correoEmisor: 'anuncio@gmail.com',
+          asunto: 'Promociones',
+          mensaje: 'Garantía extendida',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: true
+     },
+     {
+          emisor: 'Anuncio',
+          correoEmisor: 'anuncio@gmail.com',
+          asunto: 'Promociones',
+          mensaje: 'Cupones',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: true
+     },
+     {
+          emisor: 'Anuncio',
+          correoEmisor: 'anuncio@gmail.com',
+          asunto: 'Promociones',
+          mensaje: 'Puntos por compras',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: true
+     },
+     {
+          emisor: 'Anuncio',
+          correoEmisor: 'anuncio@gmail.com',
+          asunto: 'Promociones',
+          mensaje: 'Mayor porcentaje de producto',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: true
+     },
+     {
+          emisor: 'Borrar',
+          correoEmisor: 'borrar@gmail.com',
+          asunto: 'Promociones',
+          mensaje: 'Mayor porcentaje de producto',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Borrar',
+          correoEmisor: 'borrar@gmail.com',
+          asunto: 'Promociones',
+          mensaje: 'Mayor porcentaje de producto',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     },
+     {
+          emisor: 'Borrar',
+          correoEmisor: 'borrar@gmail.com',
+          asunto: 'Promociones',
+          mensaje: 'Mayor porcentaje de producto',
+          hora: '10:00am',
+          leido: false,
+          destacado: false,
+          spam: false
+     }
+];
+
+var papelera = [];
+var enviados = [];
+
+
+// console.log(recibidos);
+
+window.onload = function(){
+     $("#redactar").hide();
+     fenviados();
+     imprimirRecibidos();
+}
+
+
+// verificar el localStorage si esta vacio
+if(localStorage.getItem('recibidos') == null){
+     localStorage.setItem('recibidos', JSON.stringify(recibidos));
+}
+if(localStorage.getItem('papelera') == null){
+     localStorage.setItem('papelera', JSON.stringify(papelera));
+}
+if(localStorage.getItem('enviados') == null){
+     localStorage.setItem('enviados', JSON.stringify(enviados));
+}
+
+
+
+// Modificar estrellas y estado de "Destacado"
+function solid(id){
+     $("#sol" + id).show();
+     $("#reg" + id).hide();
+     
+     var b2 = JSON.parse(localStorage.getItem('recibidos'));
+     b2[id].destacado = true;
+     localStorage.setItem('recibidos', JSON.stringify(b2));
+}
+
+function regular(id){
+     $("#reg" + id).show(); 
+     $("#sol" + id).hide();
+
+     var b2 = JSON.parse(localStorage.getItem('recibidos'));
+     b2[id].destacado = false;
+     localStorage.setItem('recibidos', JSON.stringify(b2));
+}
+
+
+
+
+// Imprimir los correos recibidos
+function imprimirRecibidos() {
+     var r = JSON.parse(localStorage.getItem('recibidos'));
+     $("#vistaCorreos").html('<hr>');
+
+     for (let i = 0; i < r.length; i++) { 
+          
+          var color
+          if (r[i].leido == false) {
+               color = "background-color: #fafafa; font-weight: bold"     
+          }else{
+               color = "background-color: #fafafa00; font-weight: 1"
+          }
+
+          if (r[i].spam == false){
+               $("#vistaCorreos").append(`
+                    <hr>
+                    <div class="grid-vistaCorreo recibidos" style="${color}">
+                         <div id="estrellas">
+                              <i id="reg${i}" class="fa-regular fa-star" onclick="solid('${i}')"></i>
+                              <i id="sol${i}" class="fa-solid fa-star estrellaSolida" onclick="regular('${i}')"></i>
+                         </div>
+                         <i class="fa-solid fa-triangle-exclamation"></i>
+                         <p>${r[i].emisor}</p>
+                         <p>${r[i].asunto}</p>
+                         <p> - ${r[i].mensaje.substring(0,50)}...</p>
+                         <p>${r[i].hora}</p>
+                         <i class="fa-regular fa-trash-can papelera" onclick="eliminar('${i}'), imprimirRecibidos()"></i>
+                    </div>
+               `);
+          }
+
+
+          $(`#reg${i}`).hide(); // oculto
+          $(`#sol${i}`).hide(); // oculto
+
+          if (r[i].destacado == true) {
+               $(`#sol${i}`).show(); // visible
+          }else{
+               $(`#reg${i}`).show(); // visible
+          }  
+     }          
+} 
+     
+// Mensajes Destacados
+function destacados() { 
+     $("#vistaCorreos").html('<hr>');   
+     $("#vistaCorreos").append(
+          `<hr>
+          Correos destacados`
+          );
+
+          var r = JSON.parse(localStorage.getItem('recibidos'));
+          $("#vistaCorreos").html('');
+     
+          for (let i = 0; i < r.length; i++) { 
+               
+               var color
+               if (r[i].leido == false) {
+                    color = "background-color: #fafafa; font-weight: bold"     
+               }else{
+                    color = "background-color: #fafafa00; font-weight: 1"
+               }
+
+               if (r[i].destacado == true){
+
+                    $("#vistaCorreos").append(`
+                         <hr>
+                         <div class="grid-vistaCorreo recibidos" style="${color}">
+                              <div id="estrellas">
+                                   <i id="reg${i}" class="fa-regular fa-star" onclick="solid('${i}')"></i>
+                                   <i id="sol${i}" class="fa-solid fa-star estrellaSolida" onclick="regular('${i}')"></i>
+                              </div>
+                              <i class="fa-solid fa-triangle-exclamation"></i>
+                              <p>${r[i].emisor}</p>
+                              <p>${r[i].asunto}</p>
+                              <p> - ${r[i].mensaje.substring(0,50)}...</p>
+                              <p>${r[i].hora}</p>
+                              
+                         </div>
+                    `);
+               }
+     
+     
+               $(`#reg${i}`).hide(); // oculto
+               $(`#sol${i}`).hide(); // oculto
+     
+               if (r[i].destacado == true) {
+                    $(`#sol${i}`).show(); // visible
+               }else{
+                    $(`#reg${i}`).show(); // visible
+               }  
+          }     
+}
+
+
+// Mensajes Spam
+function spam() { 
+     var r = JSON.parse(localStorage.getItem('recibidos'));
+     $("#vistaCorreos").html('<hr>');
+
+     for (let i = 0; i < r.length; i++) { 
+          
+          var color
+          if (r[i].leido == false) {
+               color = "background-color: #fafafa; font-weight: bold"     
+          }else{
+               color = "background-color: #fafafa00; font-weight: 1"
+          }
+
+          if (r[i].spam == true){
+               $("#vistaCorreos").append(`
+                    <hr>
+                    <div class="grid-vistaCorreo spam" style="${color}">
+                         <i class="fa-solid fa-triangle-exclamation"></i>
+                         <p>${r[i].emisor}</p>
+                         <p>${r[i].asunto}</p>
+                         <p> - ${r[i].mensaje.substring(0,50)}...</p>
+                         <p>${r[i].hora}</p>
+                         <i class="fa-regular fa-trash-can papelera" onclick="eliminar('${i}'), spam()"></i>
+                    </div>
+               `);
+          }
+     }          
+}
+
+function fpapelera(){
+     $("#vistaCorreos").html('<hr>');
+     var r = JSON.parse(localStorage.getItem('papelera'));
+
+     for (let i = 0; i < r.length; i++) { 
+          
+          var color
+          if (r[i].leido == false) {
+               color = "background-color: #fafafa; font-weight: bold"     
+          }else{
+               color = "background-color: #fafafa00; font-weight: 1"
+          }
+
+          $("#vistaCorreos").append(`
+               <hr>
+               <div class="grid-vistaCorreo recibidos" style="${color}">
+                    <div id="estrellas">
+                         <i id="reg${i}" class="fa-regular fa-star" onclick="solid('${i}')"></i>
+                         <i id="sol${i}" class="fa-solid fa-star estrellaSolida" onclick="regular('${i}')"></i>
+                    </div>
+                    <i class="fa-solid fa-triangle-exclamation"></i>
+                    <p>${r[i].emisor}</p>
+                    <p>${r[i].asunto}</p>
+                    <p> - ${r[i].mensaje.substring(0,50)}...</p>
+                    <p>${r[i].hora}</p>
+               </div>
+          `);
+
+          $(`#reg${i}`).hide(); // oculto
+          $(`#sol${i}`).hide(); // oculto
+
+          if (r[i].destacado == true) {
+               $(`#sol${i}`).show(); // visible
+          }else{
+               $(`#reg${i}`).show(); // visible
+          }  
+     }        
+}
+
+
+// Eliminar Correos
+function eliminar(id) { 
+     var temp = JSON.parse(localStorage.getItem('recibidos'));
+
+     email = {
+          emisor: temp[id].emisor,
+          correoEmisor: temp[id].correoEmisor,
+          asunto: temp[id].asunto,
+          mensaje: temp[id].mensaje,
+          hora: temp[id].hora,
+          leido: temp[id].leido,
+          destacado: temp[id].destacado,
+          spam: temp[id].spam
+     }
+
+     papelera.push(email);
+     localStorage.setItem('papelera', JSON.stringify(papelera));
+
+     temp.splice(id, 1);
+     localStorage.setItem('recibidos', JSON.stringify(temp));
+}
+
+
+
+
+
+
+
+function enviar(){
+     email = {
+          receptor: document.getElementById('f-receptor').value,
+          emailReceptor: document.getElementById('f-emailReceptor').value,
+          asunto: document.getElementById('f-asunto').value,
+          mensaje: document.getElementById('f-mensaje').value,
+          hora: '11:00am'
+     }
+     enviados.push(email);
+     localStorage.setItem('enviados', JSON.stringify(enviados));
+
+     document.getElementById('f-receptor').value = '';
+     document.getElementById('f-emailReceptor').value = '';
+     document.getElementById('f-asunto').value = '';
+     document.getElementById('f-mensaje').value = '';
+     fenviados();
+     $("#redactar").hide();
+}
+
+function fenviados() { 
+     $("#vistaCorreos").html('<hr>');
+     
+     var r = JSON.parse(localStorage.getItem('enviados'));
+     // console.log(r);
+     for (let i = 0; i < r.length; i++) { 
+          
+          
+
+               $("#vistaCorreos").append(`
+                    <hr>
+                    <div class="grid-vistaCorreo enviados">
+                         
+                         <i class="fa-solid fa-paper-plane"></i>
+                         <p>De: ${r[i].receptor}</p>
+                         <p>Para: ${r[i].emailReceptor}</p>
+                         <p>Asundo: ${r[i].asunto.substring(0,15)}...</p>
+                         <p> - ${r[i].mensaje.substring(0,50)}...</p>
+                         <p>${r[i].hora}</p>
+                         <i class="fa-regular fa-trash-can papelera" onclick="eliminarEnviados('${i}')"></i>
+                    </div>
+               `);
+     }
+
+}
+
+// eliminar correo enviados
+function eliminarEnviados(id){
+     let temp = JSON.parse(localStorage.getItem('enviados'));
+     temp.splice(id, 1);
+     localStorage.setItem('enviados', JSON.stringify(temp));
+     fenviados();
+}
+
+function redactar(){
+     $("#redactar").show();
+}
+
+function cerrar(){
+     $("#redactar").hide();
+}
